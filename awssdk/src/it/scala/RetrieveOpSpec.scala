027b2f75a40986bd084f022447a83116d3c8956f
d2a4u <d2a4u@users.noreply.github.com>
2020-10-15 23:12:32 +0100

    Fix tests

--- awssdk/src/it/scala/RetrieveOpSpec.scala
+++ awssdk/src/it/scala/RetrieveOpSpec.scala
@@ -1,21 +1,12 @@
 package meteor
 
+import cats.effect.IO
 import cats.implicits._
-import cats.effect.{ContextShift, IO, Resource, Timer}
 import meteor.Util.resource
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-import scala.concurrent.ExecutionContext.global
 import scala.concurrent.duration._
 
-class RetrieveOpSpec
-    extends AnyFlatSpec
-    with Matchers
-    with ScalaCheckDrivenPropertyChecks {
-  implicit val timer: Timer[IO] = IO.timer(global)
-  implicit val cs: ContextShift[IO] = IO.contextShift(global)
+class RetrieveOpSpec extends ITSpec {
 
   behavior.of("retrieve operation")
   val tableName = Table("test_primary_keys")
@@ -36,15 +27,55 @@ class RetrieveOpSpec
       src.use[IO, List[TestData]] {
         case (_, client) =>
           val retrieval = client.retrieve[TestData, Id, Range](
-            Query(partitionKey, SortKeyQuery.Empty[Range]()),
+            Query(partitionKey, SortKeyQuery.Empty[Range](), Expression.empty),
             tableName,
             consistentRead = false,
             None
           ).compile.toList
-          Util.retryOf[IO, List[TestData]](retrieval, 1.second, 10)(_.size == 2)
-      }.unsafeRunSync() should contain theSameElementsAs expect
+          Util.retryOf[IO, List[TestData]](retrieval, 1.second, 10)(
+            _.size == expect.length
+          )
+      }.unsafeToFuture().futureValue should contain theSameElementsAs expect
   }
 
+//  it should "filter result by filter expression" in forAll {
+//    test: List[TestData] =>
+//      val partitionKey = Id("def")
+//      val expect = test.filter(t => t.bool && t.int > 0)
+//
+//      val src = for {
+//        client <- Client.resource[IO]
+//        r <- resource[IO, List, TestData, TestData](
+//          expect,
+//          t => client.put[TestData](t, tableName).as(t),
+//          t => client.delete(t.id, t.range, tableName)
+//        )
+//      } yield (r, client)
+//      src.use[IO, List[TestData]] {
+//        case (_, client) =>
+//          val retrieval = client.retrieve[TestData, Id, Range](
+//            Query(
+//              partitionKey,
+//              SortKeyQuery.Empty[Range](),
+//              Expression(
+//                "#b = :bool and #i > :int",
+//                Map("#b" -> "bool", "#i" -> "int"),
+//                Map(
+//                  ":bool" -> Encoder[Boolean].write(true),
+//                  ":int" -> Encoder[Int].write(0)
+//                )
+//              )
+//            ),
+//            tableName,
+//            consistentRead = false,
+//            None
+//          ).compile.toList
+//          Util.retryOf[IO, List[TestData]](retrieval, 1.second, 10)(
+//            _.size == expect.length
+//          )
+//      }.unsafeToFuture().futureValue should contain theSameElementsAs expect
+//  }
+//
   it should "limit internal requests" in forAll {
     (test1: TestData, test2: TestData) =>
       val partitionKey = Id("def")
@@ -61,14 +92,14 @@ class RetrieveOpSpec
       val result = src.use[IO, List[fs2.Chunk[TestData]]] {
         case (_, client) =>
           val retrieval = client.retrieve[TestData, Id, Range](
-            Query(partitionKey, SortKeyQuery.Empty[Range]()),
+            Query(partitionKey, SortKeyQuery.Empty[Range](), Expression.empty),
             tableName,
             consistentRead = false,
             None,
             1
           ).chunks.compile.toList
-          Util.retryOf(retrieval, 1.second, 10)(_.size == 2)
-      }.unsafeRunSync()
+          Util.retryOf(retrieval, 1.second, 10)(_.size == expect.length)
+      }.unsafeToFuture().futureValue
       result.forall(_.size == 1) shouldBe true
   }
 }
