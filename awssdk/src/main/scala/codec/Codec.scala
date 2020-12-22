3e4717f054b121bac0b3844c4e6f268aaf480377
Joe Darby <joedarby87@gmail.com>
2020-12-22 18:17:50 +0000

    Add Codec trait for easier creation of derived Encoder/Decoder (#17)
    Add codec trait with iso method for easier handling of derived classes

---
+++ awssdk/src/main/scala/codec/Codec.scala
@@ -0,0 +1,34 @@
+package meteor
+package codec
+
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+
+trait Codec[A] extends Decoder[A] with Encoder[A]
+
+object Codec {
+
+  def apply[A](implicit codec: Codec[A]): Codec[A] = codec
+
+  implicit def dynamoCodecFromEncoderAndDecoder[A](
+    implicit encoder: Encoder[A],
+    decoder: Decoder[A]
+  ): Codec[A] =
+    new Codec[A] {
+      override def write(a: A): AttributeValue = encoder.write(a)
+
+      override def read(av: AttributeValue): Either[DecoderFailure, A] =
+        decoder.read(av)
+    }
+
+  def iso[A: Codec, B](fa: A => B)(fb: B => A): Codec[B] =
+    new Codec[B] {
+      override def read(av: AttributeValue): Either[DecoderFailure, B] = {
+        Codec[A].read(av).map(fa)
+      }
+
+      override def write(b: B): AttributeValue = {
+        Codec[A].write(fb(b))
+      }
+    }
+
+}
