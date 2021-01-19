66489b4ab971a34e60e2c60169a191c294f2ad50
d2a4u <d2a4u@users.noreply.github.com>
2021-01-19 17:02:51 +0000

    Introduce getOpt syntax
    Changed `getAs` behaviour to make it fails when the key doesn't exist
    rather than default to nul(true). This breaks Decoder[Option[A]] because
    it depended on the fact that `getAs` returned `nul(true)` on missing
    key. Hence, `getOpt` syntax helps dealing with `nul(true)` and missing
    key more explicitly.

--- awssdk/src/main/scala/syntax.scala
+++ awssdk/src/main/scala/syntax.scala
@@ -15,18 +15,28 @@ trait syntax {
 
     def as[A: Decoder]: Either[DecoderError, A] = Decoder[A].read(av)
 
+    def asOpt[A: Decoder]: Either[DecoderError, Option[A]] =
+      if (Option(av.nul()).exists(_.booleanValue())) None.asRight
+      else av.as[A].map(_.some)
+
     def get(key: String): Either[DecoderError, AttributeValue] =
-      if (av.hasM()) {
-        av.m().getOrDefault(
-          key,
-          AttributeValue.builder().nul(true).build()
-        ).asRight[DecoderError]
+      if (av.hasM) {
+        Option(av.m().get(key)).fold(
+          DecoderError.missingKeyFailure(key).asLeft[AttributeValue]
+        )(_.asRight[DecoderError])
       } else {
         DecoderError.invalidTypeFailure(DynamoDbType.M).asLeft[AttributeValue]
       }
 
     def getAs[A: Decoder](key: String): Either[DecoderError, A] =
       get(key).flatMap(_.as[A])
+
+    def getOpt[A: Decoder](key: String): Either[DecoderError, Option[A]] =
+      if (av.hasM) {
+        Option(av.m().get(key)).fold(none[A].asRight[DecoderError])(_.asOpt[A])
+      } else {
+        DecoderError.invalidTypeFailure(DynamoDbType.M).asLeft[Option[A]]
+      }
   }
 }
 
